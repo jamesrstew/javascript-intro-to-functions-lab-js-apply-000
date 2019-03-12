7bcc4d62ae3cc89a113e9e2b18eeb0748171dd1a
James Stewart <james.stewart@blackrock.com>
2019-03-12 17:23:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ function logWhisper(string) {
 logWhisper("Hello")
 
 function sayHiToGrandma(string) {
+  let person = "Grandma"
   if (string == string.toLowerCase()) {
     return "I can't hear you!";
   }
   else if (string == string.toUpperCase()) {
     return "YES INDEED!";
+  }
+  else if (string == "I love you, ${person}") {
+    return "I love you, "+ " " + "too."
   }
 }
 
