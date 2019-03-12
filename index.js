d02c709f625b07aa39af0c131c4249a10ac3e810
James Stewart <james.stewart@blackrock.com>
2019-03-12 17:01:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+shout("hello")
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+whisper("HELLO")
+
+function logShout(spy) {
+  let turtle = spy.toUpperCase()
+  console.log(turtle)
+}
+
+logShout("hello")
+
+function logWhisper(string) {
+  let spy = string.toLowerCase()
+  console.log(spy)
+}
+
+logWhisper("Hello")
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    string.toUpperCase()
+    console.log("YES INDEED!")
+  }
+  else {
+    string.toLowerCase()
+    console.log("I can't hear you!")
+  }
+
+}
+
+sayHiToGrandma("Hello")
